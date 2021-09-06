d27d2b9e4760dbfecd28db02bb35ebc5b291768d
zhongweiyue <398174785@qq.com>
2021-09-06 22:14:16 +0800

    更新资源

--- Src/Server/GameServer/GameServer/GameServer.cs
+++ Src/Server/GameServer/GameServer/GameServer.cs
@@ -24,10 +24,8 @@ namespace GameServer
         {
             network = new NetService();
             network.Init(8000);
-            HelloWorldService.Instance.Init();
-            //DBService.Instance.Init();
-            //var a = DBService.Instance.Entities.Characters.Where(s => s.TID == 1);
-            //Console.WriteLine("{0}",a.FirstOrDefault<TCharacter>().Name);
+            DBService.Instance.Init();
+            UserService.Instance.Init();
             thread = new Thread(new ThreadStart(this.Update));
 
             return true;
@@ -36,7 +34,6 @@ namespace GameServer
         public void Start()
         {
             network.Start();
-            HelloWorldService.Instance.Start();
             running = true;
             thread.Start();
         }
