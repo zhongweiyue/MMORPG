834d2b55b4ed095e16ec2f02abc384fb545d34b1
zhongweiyue <398174785@qq.com>
2021-09-06 21:18:13 +0800

    网络消息处理

---
+++ Src/Client/Assets/Scripts/Test/Login.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour
+{
+    private void Start()
+    {
+        Network.NetClient.Instance.Init("127.0.0.1", 8000);
+        Network.NetClient.Instance.Connect();
+
+        SkillBridge.Message.NetMessage msg = new SkillBridge.Message.NetMessage();
+        msg.Request = new SkillBridge.Message.NetMessageRequest();
+        msg.Request.firstRequest = new SkillBridge.Message.FirstTestRequest();
+        msg.Request.firstRequest.Helloword = "helloworld";
+        Network.NetClient.Instance.SendMessage(msg);
+    }
+}
---
+++ Src/Server/GameServer/GameServer/Services/HelloWorldService.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using Network;
+using SkillBridge.Message;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class HelloWorldService:Singleton<HelloWorldService>
+{
+    public void Init()
+    {
+
+    }
+
+    public void Start()
+    {
+        MessageDistributer<NetConnection<NetSession>>.Instance.Subscribe<FirstTestRequest>(this.OnFirstTestRequest);
+    }
+
+    void OnFirstTestRequest(NetConnection<NetSession>sender,FirstTestRequest request)
+    {
+        Log.InfoFormat("OnFirstTestRequest helloword:{0}", request.Helloword);
+    }
+}
+
