945f364c735648d95736b42ce8fb991134e70ae0
zhongweiyue <zhongweiyue@sl916.com>
2021-09-13 09:56:46 +0800

    UILogin脚本

---
+++ Src/Client/Assets/Scripts/UI/UILogin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILogin : MonoBehaviour
+{
+    public Button loginBtn;
+    public InputField username;
+    public InputField password;
+
+    private void Start()
+    {
+        
+    }
+
+
+}
