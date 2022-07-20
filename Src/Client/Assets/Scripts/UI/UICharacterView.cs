69b62eef0fba04db84260b94ded62679114a1a71
zhongweiyue <398174785@qq.com>
2022-07-21 00:41:29 +0800

    [创建角色]配置表的读取

---
+++ Src/Client/Assets/Scripts/UI/UICharacterView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICharacterView : MonoBehaviour
+{
+
+    public GameObject[] characters;
+    private int currentCharacter = 0;
+    public int CurrentCharacter
+    {
+        get
+        {
+            return currentCharacter;
+        }
+        set
+        {
+            currentCharacter = value;
+            UpdateCharacter();
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void UpdateCharacter()
+    {
+        for (int i = 0; i < characters.Length; i++)
+        {
+            characters[i].SetActive( i == currentCharacter);
+        }
+    }
+}
