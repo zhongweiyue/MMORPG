a55ee5f7c6bcea0d221d50bd8d899d1d8fdcb506
zhongweiyue <398174785@qq.com>
2022-09-20 09:34:41 +0800

    [背包系统]配置和资源

--- Src/Server/GameServer/GameServer/Entities.edmx.sql
+++ Src/Server/GameServer/GameServer/Entities.edmx.sql
@@ -2,7 +2,7 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 09/17/2022 12:05:02
+-- Date Created: 09/20/2022 09:11:14
 -- Generated from EDMX file: E:\unity project\MMORPG\MyMMO\MMORPG\Src\Server\GameServer\GameServer\Entities.edmx
 -- --------------------------------------------------
 
@@ -74,7 +74,8 @@ CREATE TABLE [dbo].[Characters] (
     [MapPosX] int  NOT NULL,
     [MapPosY] int  NOT NULL,
     [MapPosZ] int  NOT NULL,
-    [Player_ID] int  NOT NULL
+    [Player_ID] int  NOT NULL,
+    [Bag_Id] int  NOT NULL
 );
 GO
 
@@ -84,6 +85,14 @@ CREATE TABLE [dbo].[CharacterItem] (
     [ItemID] int  NOT NULL,
     [CharacterID] int  NOT NULL,
     [ItemCount] int  NOT NULL
+);
+GO
+
+-- Creating table 'TCharacterBags'
+CREATE TABLE [dbo].[TCharacterBags] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Items] varbinary(max)  NOT NULL,
+    [Unlocked] int  NOT NULL
 );
 GO
 
@@ -112,6 +121,12 @@ GO
 -- Creating primary key on [Id] in table 'CharacterItem'
 ALTER TABLE [dbo].[CharacterItem]
 ADD CONSTRAINT [PK_CharacterItem]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'TCharacterBags'
+ALTER TABLE [dbo].[TCharacterBags]
+ADD CONSTRAINT [PK_TCharacterBags]
     PRIMARY KEY CLUSTERED ([Id] ASC);
 GO
 
@@ -162,6 +177,21 @@ GO
 CREATE INDEX [IX_FK_TCharacterTCharacterItem]
 ON [dbo].[CharacterItem]
     ([CharacterID]);
+GO
+
+-- Creating foreign key on [Bag_Id] in table 'Characters'
+ALTER TABLE [dbo].[Characters]
+ADD CONSTRAINT [FK_CharacterBag]
+    FOREIGN KEY ([Bag_Id])
+    REFERENCES [dbo].[TCharacterBags]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_CharacterBag'
+CREATE INDEX [IX_FK_CharacterBag]
+ON [dbo].[Characters]
+    ([Bag_Id]);
 GO
 
 -- --------------------------------------------------
