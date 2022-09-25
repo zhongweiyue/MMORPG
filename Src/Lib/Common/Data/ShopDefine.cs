5fb3cfea80289cf9516d348df12574c4daa752b3
zhongweiyue <398174785@qq.com>
2022-09-26 00:35:45 +0800

    【商店系统】配置文件提交

---
+++ Src/Lib/Common/Data/ShopDefine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Data
+{
+    public class ShopDefine
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+        public int Status { get; set; }
+    }
+}
